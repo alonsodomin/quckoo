ce1521031bdd315ace3a22d77810ee2ddce1a1ac
alonsodomin <alonso.domin@gmail.com>
2015-07-27 09:54:12 +0100

    Testing with futures.

--- scheduler/src/main/scala/io/chronos/scheduler/internal/DistributedJobRegistry.scala
+++ scheduler/src/main/scala/io/chronos/scheduler/internal/DistributedJobRegistry.scala
@@ -6,8 +6,9 @@ import io.chronos.scheduler.JobRegistry
 import org.apache.ignite.Ignite
 import org.apache.ignite.lang.{IgniteFuture, IgniteInClosure}
 
-import scala.concurrent.{ExecutionContext, Future, Promise}
-import scala.util.Success
+import scala.collection.JavaConversions._
+import scala.concurrent._
+import scala.util.Try
 
 /**
  * Created by aalonsodominguez on 26/07/15.
@@ -15,27 +16,22 @@ import scala.util.Success
 trait DistributedJobRegistry extends JobRegistry {
   protected implicit val ignite: Ignite
 
-  private val jobSpecCache = ignite.getOrCreateCache[JobId, JobSpec]("jobSpecCache").withAsync()
+  private val jobSpecCache = ignite.getOrCreateCache[JobId, JobSpec]("jobSpecCache")
 
-  override def availableJobSpecs(implicit ec: ExecutionContext): Future[Seq[JobSpec]] = ???
+  override def availableJobSpecs(implicit ec: ExecutionContext): Future[Seq[JobSpec]] =
+    Future { jobSpecCache.getAll(Set.empty).values().toSeq }
 
-  override def specById(jobId: JobId)(implicit ec: ExecutionContext): Future[Option[JobSpec]] = {
-    implicit val async = jobSpecCache
-    jobSpecCache.get(jobId)
-
-    jobSpecCache.future[JobSpec]().map(Option(_))
-  }
+  override def specById(jobId: JobId)(implicit ec: ExecutionContext): Future[Option[JobSpec]] = Future { Option(jobSpecCache.get(jobId)) }
 
   override def registerJobSpec(jobSpec: JobSpec)(implicit ec: ExecutionContext): Future[JobId] = {
-    val promise = Promise[JobId]()
     jobSpecCache.put(jobSpec.id, jobSpec)
+    Future { jobSpec.id }
+  }
 
-    val internalFuture = jobSpecCache.future()
-    internalFuture.listen(new IgniteInClosure[IgniteFuture[Nothing]] {
-      override def apply(e: IgniteFuture[Nothing]): Unit =
-        promise.complete(Success(jobSpec.id))
-    })
-    promise.future
+  private class IgniteFutureListener[A, B](promise: Promise[B], f: A => B) extends IgniteInClosure[IgniteFuture[A]] {
+    override def apply(e: IgniteFuture[A]): Unit = {
+      promise.complete(Try(e.get()).map(f(_)))
+    }
   }
 
 }
--- scheduler/src/test/scala/io/chronos/scheduler/internal/DistributedJobRegistryTest.scala
+++ scheduler/src/test/scala/io/chronos/scheduler/internal/DistributedJobRegistryTest.scala
@@ -5,32 +5,49 @@ import java.util.UUID
 import io.chronos.JobSpec
 import io.chronos.id.JobModuleId
 import org.apache.ignite.{Ignite, Ignition}
-import org.scalatest.{BeforeAndAfter, FlatSpec}
+import org.scalatest._
+import org.scalatest.concurrent.ScalaFutures
 
 /**
  * Created by aalonsodominguez on 27/07/15.
  */
-class DistributedJobRegistryTest extends FlatSpec with BeforeAndAfter {
+class DistributedJobRegistryTest extends FlatSpec with BeforeAndAfter with Matchers with ScalaFutures {
+  import scala.concurrent.ExecutionContext.Implicits.global
 
   class DistributedJobRegistryImpl(val ignite: Ignite) extends DistributedJobRegistry
 
-  var mockIgnite: Ignite = _
+  var ignite: Ignite = _
   var jobRegistry: DistributedJobRegistry = _
 
+  val jobSpec = JobSpec(
+    id = UUID.randomUUID(),
+    displayName = "Foo",
+    moduleId = JobModuleId("com.example", "bar", "SNAPSHOT"),
+    jobClass = "com.example.Bar"
+  )
+
   before {
-    mockIgnite = Ignition.ignite()
-    jobRegistry = new DistributedJobRegistryImpl(mockIgnite)
+    ignite = Ignition.start()
+    jobRegistry = new DistributedJobRegistryImpl(ignite)
   }
 
   "A distributed job registry" should "persist job specs asynchronously" in {
-    val jobSpec = JobSpec(
-      id = UUID.randomUUID(),
-      displayName = "Foo",
-      moduleId = JobModuleId("com.example", "bar", "SNAPSHOT"),
-      jobClass = "com.example.Bar"
-    )
+    whenReady(jobRegistry.registerJobSpec(jobSpec)) { result =>
+      result should be (jobSpec.id)
+    }
+  }
 
-    jobRegistry.registerJobSpec(jobSpec)
+  it should "return the previously registered job spec" in {
+    whenReady(jobRegistry.specById(jobSpec.id)) {
+      case Some(returnedSpec) =>
+        returnedSpec should be (jobSpec)
+    }
+  }
+
+  it should "return the registered job spec when available jobs are retrieved" in {
+    whenReady(jobRegistry.availableJobSpecs) { result =>
+      result should contain (jobSpec)
+    }
   }
 
 }
