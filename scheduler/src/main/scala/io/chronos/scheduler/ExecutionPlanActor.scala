b2887fa8a7488783996cbfdfa6f08fd0da0f3b8a
alonsodomin <alonso.domin@gmail.com>
2015-07-30 11:09:45 +0100

    Removing the separation between execution queue and execution plan

--- scheduler/src/main/scala/io/chronos/scheduler/ExecutionPlanActor.scala
+++ scheduler/src/main/scala/io/chronos/scheduler/ExecutionPlanActor.scala
@@ -8,12 +8,14 @@ import akka.contrib.pattern.{DistributedPubSubExtension, DistributedPubSubMediat
 import akka.pattern._
 import akka.util.Timeout
 import io.chronos.Trigger.{LastExecutionTime, ReferenceTime, ScheduledTime}
+import io.chronos._
 import io.chronos.id._
+import io.chronos.protocol.WorkerProtocol.{RegisterWorker, RequestWork, WorkReady}
 import io.chronos.protocol.{SchedulerProtocol, _}
-import io.chronos.{Execution, JobSchedule, JobSpec, topic}
 import org.apache.ignite.Ignite
 
 import scala.collection.JavaConversions._
+import scala.concurrent.Future
 import scala.concurrent.duration._
 
 /**
@@ -22,19 +24,22 @@ import scala.concurrent.duration._
 object ExecutionPlanActor {
 
   private case object Heartbeat
+  private case object CleanupBeat
 
 }
 
-class ExecutionPlanActor(ignite: Ignite, heartbeatInterval: FiniteDuration, sweepBatchLimit: Int)(implicit clock: Clock)
+class ExecutionPlanActor(ignite: Ignite, heartbeatInterval: FiniteDuration, sweepBatchLimit: Int, queueCapacity: Int, maxWorkTimeout: FiniteDuration)(implicit clock: Clock)
   extends Actor with ActorLogging {
 
   import ExecutionPlanActor._
   import SchedulerProtocol._
   import context.dispatcher
 
-  val mediator = DistributedPubSubExtension(context.system).mediator
-  mediator ! DistributedPubSubMediator.Subscribe(topic.Executions, self)
+  // References to other actors in the system
+  private val mediator = DistributedPubSubExtension(context.system).mediator
+  private val registry = context.system.actorSelection(context.system / "registry")
 
+  // Distributed data structures
   private val beating = ignite.atomicReference("beating", false, true)
 
   private val scheduleCounter = ignite.atomicSequence("scheduleCounter", 0, true)
@@ -44,35 +49,86 @@ class ExecutionPlanActor(ignite: Ignite, heartbeatInterval: FiniteDuration, swee
   private val executionMap = ignite.getOrCreateCache[ExecutionId, Execution]("executions")
   private val executionBySchedule = ignite.getOrCreateCache[ScheduleId, ExecutionId]("executionBySchedule")
 
-  private val registryRef = context.system.actorSelection(context.system / "registry")
-  private val queueRef = context.system.actorSelection(context.system / "execution" / "queue")
+  private val executionQueue = ignite.queue[ExecutionId]("executionQueue", queueCapacity, null)
+
+  // Tasks
+  private val cleanupTask = context.system.scheduler.schedule(maxWorkTimeout / 2, maxWorkTimeout / 2, self, CleanupBeat)
+  private val heartbeatTask = context.system.scheduler.schedule(0 seconds, heartbeatInterval, self, Heartbeat)
+
+  // Non-persistent state
+  private var workers = Map.empty[WorkerId, WorkerState]
+
+  override def postStop(): Unit = {
+    cleanupTask.cancel()
+    heartbeatTask.cancel()
+  }
 
   def receive = {
     case ScheduleJob(schedule) =>
       implicit val timeout = Timeout(5 seconds)
-      (registryRef ? GetJobSpec(schedule.jobId)).mapTo[Option[JobSpec]].map {
+      (registry ? GetJobSpec(schedule.jobId)).mapTo[Option[JobSpec]].map {
         case Some(jobSpec) =>
           val scheduleId = (schedule.jobId, scheduleCounter.incrementAndGet())
           scheduleMap.put(scheduleId, schedule)
-          ScheduleJobAck(defineExecutionFor(scheduleId).executionId)
+          val execution = defineExecutionFor(scheduleId)
+          mediator ! DistributedPubSubMediator.Publish(topic.Executions, ExecutionEvent(execution.executionId, execution.stage))
+          ScheduleJobAck(execution.executionId)
 
         case _ => ScheduleJobFailed(Left(JobNotRegistered(schedule.jobId)))
       } recover {
         case e: Throwable => ScheduleJobFailed(Right(e))
-      } pipeTo sender()
+      } pipeTo sender
 
     case GetSchedule(scheduleId) =>
       sender ! Option(scheduleMap.get(scheduleId))
 
-    case ExecutionEvent(executionId, stage: Execution.Started) =>
-      val execution = executionMap.invoke(executionId, new EntryProcessor[ExecutionId, Execution, Execution] {
-        override def process(entry: MutableEntry[((JobId, Long), Long), Execution], arguments: AnyRef*): Execution = {
-          val execution = entry.getValue << stage
-          entry.setValue(execution)
-          execution
+    case RegisterWorker(workerId) =>
+      if (workers.contains(workerId)) {
+        workers += (workerId -> workers(workerId).copy(ref = sender()))
+      } else {
+        workers += (workerId -> WorkerState(sender(), status = WorkerState.Idle))
+        log.info("Worker registered: {}", workerId)
+        if (!executionQueue.isEmpty) {
+          sender ! WorkReady
         }
-      })
-      mediator ! DistributedPubSubMediator.Publish(topic.Executions, stage)
+        mediator ! DistributedPubSubMediator.Publish(topic.Workers, WorkerRegistered(workerId))
+      }
+
+    case RequestWork(workerId) if !executionQueue.isEmpty =>
+      workers.get(workerId) match {
+        case Some(workerState@WorkerState(_, WorkerState.Idle)) =>
+          val executionId = executionQueue.take()
+
+          def futureJobSpec: Future[Option[JobSpec]] = {
+            implicit val timeout = Timeout(5 seconds)
+            (registry ? GetJobSpec(executionId._1._1)).mapTo[Option[JobSpec]]
+          }
+
+          def futureSchedule: Future[Option[JobSchedule]] = Future { Option(scheduleMap.get(executionId._1)) }
+
+          futureJobSpec.zip(futureSchedule).map(x => (x._1, x._2)).map {
+            case (Some(jobSpec), Some(schedule)) =>
+              def workTimeout: Deadline = Deadline.now + schedule.timeout.getOrElse(maxWorkTimeout)
+
+              val executionStage = Execution.Started(ZonedDateTime.now(clock), workerId)
+              updateExecutionAndApply(executionId, executionStage) { execution =>
+                log.info("Delivering execution to worker. executionId={}, workerId={}", executionId, workerId)
+                sender ! Work(executionId, schedule.params, jobSpec.moduleId, jobSpec.jobClass)
+
+                workers += (workerId -> workerState.copy(status = WorkerState.Busy(executionId, workTimeout)))
+                mediator ! DistributedPubSubMediator.Publish(topic.Executions, ExecutionEvent(executionId, executionStage))
+              }
+
+            case _ =>
+              log.error("Found a queued execution with no job or schedule association. executionId={}", executionId)
+
+          } recover {
+            case cause: Throwable =>
+              log.error(cause, "Couldn't send execution to worker. Execution will be put back in the queue and retried later. executionId={}, workerId={}", executionId, workerId)
+              executionQueue.put(executionId)
+              notifyWorkers()
+          }
+      }
 
     case Heartbeat if beating.compareAndSet(false, true) =>
       var itemCount = 0
@@ -100,13 +156,28 @@ class ExecutionPlanActor(ignite: Ignite, heartbeatInterval: FiniteDuration, swee
         log.info("Placing execution into work queue. executionId={}", exec.executionId)
         val updatedExec = exec << Execution.Triggered(ZonedDateTime.now(clock))
         executionMap.put(execId, updatedExec)
-        queueRef ! ExecutionQueueActor.Enqueue(updatedExec)
+        executionQueue.put(execId)
+        mediator ! DistributedPubSubMediator.Publish(topic.Executions, ExecutionEvent(execId, updatedExec.stage))
 
         itemCount += 1  // This awful statement is there to help the upper helper functions to build the batch
       }
 
       // Reset the atomic boolean flag to allow for more "beats"
       beating.set(false)
+
+    case CleanupBeat =>
+      for ((workerId, workerState @ WorkerState(_, WorkerState.Busy(executionId, timeout))) <- workers) {
+        if (timeout.isOverdue()) {
+          log.info("Execution {} at worker {} timed out!", executionId, workerId)
+          val executionStatus = Execution.Finished(ZonedDateTime.now(clock), workerId, Execution.TimedOut)
+          updateExecutionAndApply(executionId, executionStatus) { _ =>
+            workers -= workerId
+            mediator ! DistributedPubSubMediator.Publish(topic.Workers, WorkerUnregistered(workerId))
+            mediator ! DistributedPubSubMediator.Publish(topic.Executions, ExecutionEvent(executionId, executionStatus))
+            notifyWorkers()
+          }
+        }
+      }
   }
 
   private def nextExecutionTime(scheduleId: ScheduleId, schedule: JobSchedule): Option[ZonedDateTime] =
@@ -127,6 +198,31 @@ class ExecutionPlanActor(ignite: Ignite, heartbeatInterval: FiniteDuration, swee
     executionMap.put(executionId, execution)
     executionBySchedule.put(scheduleId, executionId)
     execution
+  }
+
+  private def notifyWorkers(): Unit = {
+    if (!executionQueue.isEmpty) {
+      workers.foreach {
+        case (_, WorkerState(ref, WorkerState.Idle)) => ref ! WorkReady
+        case _ => // busy
+      }
+    }
+  }
+
+  private def changeWorkerToIdle(workerId: WorkerId, executionId: ExecutionId): Unit = workers.get(workerId) match {
+    case Some(workerState @ WorkerState(_, WorkerState.Busy(`executionId`, _))) =>
+      workers += (workerId -> workerState.copy(status = WorkerState.Idle))
+    case _ => // might happen after standby recovery, worker state is not persisted
+  }
+
+  private def updateExecutionAndApply[T](executionId: ExecutionId, stage: Execution.Stage)(f: Execution => T): Future[T] = Future {
+    executionMap.invoke(executionId, new EntryProcessor[ExecutionId, Execution, T] {
+      override def process(entry: MutableEntry[((JobId, Long), Long), Execution], arguments: AnyRef*): T = {
+        val execution = entry.getValue << stage
+        entry.setValue(execution)
+        f(execution)
+      }
+    })
   }
 
 }
