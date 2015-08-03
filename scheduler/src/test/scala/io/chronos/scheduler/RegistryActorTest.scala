6ad2dc86a375329bab3324b565d88eaf30377e3d
alonsodomin <alonso.domin@gmail.com>
2015-08-03 17:33:33 +0100

    Need to find a way to make this test work

--- scheduler/src/test/scala/io/chronos/scheduler/RegistryActorTest.scala
+++ scheduler/src/test/scala/io/chronos/scheduler/RegistryActorTest.scala
@@ -3,6 +3,7 @@ package io.chronos.scheduler
 import java.util.UUID
 
 import akka.actor.ActorSystem
+import akka.cluster.Cluster
 import akka.testkit.TestKit
 import com.typesafe.config.ConfigFactory
 import io.chronos.JobSpec
@@ -11,29 +12,46 @@ import io.chronos.resolver.ModuleResolver
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, WordSpecLike}
 
+import scala.concurrent.duration._
+
 /**
  * Created by domingueza on 03/08/15.
  */
 object RegistryActorTest {
+
+  val ClusterConfig = ConfigFactory.load("test-cluster")
+  val ClientConfig = ConfigFactory.parseString(
+    """
+      |akka.actor.provider = "akka.remote.RemoteActorRefProvider"
+      |akka.remote.netty.tcp.port=0
+    """.stripMargin)
+
   val TestModuleId = ModuleId("io.chronos", "test", "latest")
+
 }
 
 class RegistryActorTest(_system: ActorSystem) extends TestKit(_system) with WordSpecLike with BeforeAndAfter
   with BeforeAndAfterAll with MockFactory {
 
-  import RegistryActorTest._
   import io.chronos.protocol._
 
-  def this() = this(ActorSystem("RegistryActorTest", ConfigFactory.load("test-application")))
+  def this() = this(ActorSystem("RegistryActorTest", RegistryActorTest.ClusterConfig))
 
   override def afterAll: Unit = {
-    TestKit.shutdownActorSystem(system)
+    system.shutdown()
+    system.awaitTermination()
   }
 
   "A Registry actor" must {
+    import RegistryActorTest._
+
     val mockRegistry = mock[Registry]
     val mockModuleResolver = mock[ModuleResolver]
-    val registryActor = system.actorOf(RegistryActor.props(mockRegistry, mockModuleResolver))
+
+    val clusterAddress = Cluster(system).selfAddress
+    Cluster(system).join(clusterAddress)
+
+    val registry = system.actorOf(RegistryActor.props(mockRegistry, mockModuleResolver), "registry")
 
     "retrieve a job from the registry" in {
       val jobId = UUID.randomUUID()
@@ -41,8 +59,12 @@ class RegistryActorTest(_system: ActorSystem) extends TestKit(_system) with Word
 
       (mockRegistry.getJob _).expects(jobId).returning(Option(expectedJobSpec))
 
-      registryActor ! GetJob(jobId)
-      expectMsg(Option(expectedJobSpec))
+      within(10.seconds) {
+        awaitAssert {
+          registry ! GetJob(jobId)
+          expectMsg(Option(expectedJobSpec))
+        }
+      }
     }
   }
 
