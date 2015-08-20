067756212ff00a99f1afbe1a0dbb35d918b3feef
A. Alonso Dominguez <alonso.domin@gmail.com>
2015-08-21 00:09:35 +0100

    Got a more full lifecycle test for the execution plan

--- scheduler/src/test/scala/io/chronos/scheduler/execution/ExecutionPlanSpec.scala
+++ scheduler/src/test/scala/io/chronos/scheduler/execution/ExecutionPlanSpec.scala
@@ -4,12 +4,13 @@ import java.time.{Clock, Instant, ZoneId, ZonedDateTime}
 import java.util.UUID
 
 import akka.actor._
+import akka.testkit.TestActors.ForwardActor
 import akka.testkit._
 import io.chronos.id.ModuleId
 import io.chronos.scheduler.Registry
 import io.chronos.{JobSpec, Trigger}
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Inside, Matchers}
+import org.scalatest._
 
 /**
  * Created by aalonsodominguez on 20/08/15.
@@ -23,20 +24,12 @@ object ExecutionPlanSpec {
   final val TestJobSpec = JobSpec(UUID.randomUUID(), "foo", "foo desc", TestModuleId, "com.example.Job")
 
   class WatchAndForwardActor(executionPlan: ActorRef, listener: ActorRef)
-    extends Actor {
-
-    context.watch(executionPlan)
-
-    override def receive = {
-      case message => listener ! message
-    }
-
-  }
+    extends ForwardActor(listener) { context.watch(executionPlan) }
 
 }
 
 class ExecutionPlanSpec extends TestKit(ActorSystem("ExecutionPlanSpec")) with ImplicitSender
-  with FlatSpecLike with BeforeAndAfterAll with Matchers with Inside with MockFactory {
+  with WordSpecLike with BeforeAndAfterAll with Matchers with Inside with MockFactory {
 
   import ExecutionPlanSpec._
   import Trigger._
@@ -46,35 +39,65 @@ class ExecutionPlanSpec extends TestKit(ActorSystem("ExecutionPlanSpec")) with I
   override protected def afterAll(): Unit =
     TestKit.shutdownActorSystem(system)
 
-  "An execution plan" should "kill itself if job is not enabled" in {
-    val trigger = mock[Trigger]
-    val executionProps: ExecutionProps =
-      (planId, jobSpec) => TestActors.echoActorProps
+  "An execution plan" should {
 
-    val receiver = TestProbe()
-    val executionPlan = TestActorRef(ExecutionPlan.props(trigger)(executionProps))
-    TestActorRef(Props(classOf[WatchAndForwardActor], executionPlan, receiver.ref))
+    "terminate itself if job is not enabled" in {
+      val trigger = mock[Trigger]
+      val executionProps: ExecutionProps =
+        (planId, jobSpec) => TestActors.echoActorProps
 
-    executionPlan ! Registry.JobNotEnabled(UUID.randomUUID())
+      val executionPlan = TestActorRef(ExecutionPlan.props(trigger)(executionProps))
+      TestActorRef(Props(classOf[WatchAndForwardActor], executionPlan, self))
 
-    //receiver.expectTerminated(executionPlan)
+      executionPlan ! Registry.JobNotEnabled(UUID.randomUUID())
+
+      //expectTerminated(executionPlan)
+    }
+
   }
 
-  it should "create an execution from a job specification" in {
+  "An execution plan" should  {
     val triggerMock = mock[Trigger]
     val executionProbe = TestProbe()
     val executionProps: ExecutionProps =
       (planId, jobSpec) => TestActors.forwardActorProps(executionProbe.ref)
 
-    val expectedScheduleTime = ZonedDateTime.now(clock)
-    val expectedExecutionTime = expectedScheduleTime
-    (triggerMock.nextExecutionTime(_: ReferenceTime)(_: Clock)).expects(ScheduledTime(expectedScheduleTime), clock).returning(Some(expectedExecutionTime))
-
     val executionPlan = TestActorRef(ExecutionPlan.props(triggerMock)(executionProps))
+    TestActorRef(Props(classOf[WatchAndForwardActor], executionPlan, self))
 
-    executionPlan ! TestJobSpec
+    "create an execution from a job specification" in {
+      val expectedScheduleTime = ZonedDateTime.now(clock)
+      val expectedExecutionTime = expectedScheduleTime
 
-    executionProbe.expectMsg(Execution.WakeUp)
+      (triggerMock.nextExecutionTime(_: ReferenceTime)(_: Clock)).expects(ScheduledTime(expectedScheduleTime), clock).returning(Some(expectedExecutionTime))
+
+      executionPlan ! TestJobSpec
+
+      executionProbe.expectMsg(Execution.WakeUp)
+    }
+
+    "re-schedule the execution if the trigger is recurring" in {
+      val expectedLastExecutionTime = ZonedDateTime.now(clock)
+      val expectedExecutionTime = expectedLastExecutionTime
+
+      (triggerMock.isRecurring _).expects().returning(true)
+      (triggerMock.nextExecutionTime(_: ReferenceTime)(_: Clock)).expects(LastExecutionTime(expectedLastExecutionTime), clock).returning(Some(expectedExecutionTime))
+
+      executionProbe.send(executionPlan, Execution.Result(Execution.Success("bar")))
+
+      executionProbe.expectMsg(Execution.WakeUp)
+    }
+
+    "stop the execution plan if trigger returns None" in {
+      val expectedLastExecutionTime = ZonedDateTime.now(clock)
+
+      (triggerMock.isRecurring _).expects().returning(true)
+      (triggerMock.nextExecutionTime(_: ReferenceTime)(_: Clock)).expects(LastExecutionTime(expectedLastExecutionTime), clock).returning(None)
+
+      executionProbe.send(executionPlan, Execution.Result(Execution.Success("bar")))
+
+      //expectTerminated(executionPlan)
+    }
   }
 
 }
