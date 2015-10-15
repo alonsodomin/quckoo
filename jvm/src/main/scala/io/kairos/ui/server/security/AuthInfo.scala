70445df6d06987112f7a601384b136dba229fae5
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2015-10-15 08:54:08 +0100

    Encoding the auth info

--- jvm/src/main/scala/io/kairos/ui/server/security/AuthInfo.scala
+++ jvm/src/main/scala/io/kairos/ui/server/security/AuthInfo.scala
@@ -1,6 +1,7 @@
 package io.kairos.ui.server.security
 
 import io.kairos.ui.auth.UserId
+import io.kairos.ui.base64.Base64._
 
 /**
  * Created by alonsodomin on 14/10/2015.
@@ -11,5 +12,8 @@ class AuthInfo(val userId: UserId, val token: String) {
 
   def copy(token: String): AuthInfo =
     new AuthInfo(userId, token)
+
+  override def toString: String =
+    (userId + ":" + token).getBytes("UTF-8").toBase64
 
 }
