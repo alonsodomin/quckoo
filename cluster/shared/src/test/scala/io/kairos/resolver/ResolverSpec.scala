eecb87036dedc2ed5444c8b9f5b1b54ee14a3d29
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2016-01-12 19:04:17 +0000

    FIxing the ResolverSpec

--- cluster/shared/src/test/scala/io/kairos/resolver/ResolverSpec.scala
+++ cluster/shared/src/test/scala/io/kairos/resolver/ResolverSpec.scala
@@ -5,9 +5,11 @@ import java.net.URL
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
 import io.kairos.id.ArtifactId
-import io.kairos.protocol.{ResolutionFailed, UnresolvedDependencies}
+import io.kairos.protocol.{ErrorResponse, ExceptionThrown, UnresolvedDependency}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+import scalaz._
 
 /**
  * Created by domingueza on 23/08/15.
@@ -17,8 +19,11 @@ class ResolverSpec extends TestKit(ActorSystem("ResolverSpec")) with ImplicitSen
 
   import Resolver._
 
+  import Scalaz._
+
   final val TestArtifactId = ArtifactId("com.example", "foo", "latest")
-  val mockResolve = mockFunction[ArtifactId, Boolean, Either[ResolutionFailed, Artifact]]
+
+  val mockResolve = mockFunction[ArtifactId, Boolean, ResolutionResult]
   val moduleResolver = TestActorRef(Resolver.props(mockResolve))
 
   override def afterAll(): Unit =
@@ -27,65 +32,67 @@ class ResolverSpec extends TestKit(ActorSystem("ResolverSpec")) with ImplicitSen
   "A resolver" should "return the artifact when it's valid" in {
     val jobPackage = Artifact(TestArtifactId, Seq(new URL("http://www.google.com")))
 
-    mockResolve expects (TestArtifactId, false) returning Right(jobPackage)
+    mockResolve expects (TestArtifactId, false) returning jobPackage.successNel[ErrorResponse]
 
     moduleResolver ! Validate(TestArtifactId)
 
-    val returnedPackage = expectMsgType[Artifact]
-    returnedPackage should be(jobPackage)
+    val response = expectMsgType[ResolutionResult]
+    response should be(jobPackage.successNel[ErrorResponse])
   }
 
   it should "return resolution failed error if the artifactId is not valid" in {
-    val expectedResolutionFailed = UnresolvedDependencies(Seq("com.foo.bar"))
+    val expectedResolutionFailed = UnresolvedDependency(TestArtifactId)
 
-    mockResolve expects (TestArtifactId, false) returning Left(expectedResolutionFailed)
+    mockResolve expects (TestArtifactId, false) returning expectedResolutionFailed.failureNel[Artifact]
 
     moduleResolver ! Validate(TestArtifactId)
 
-    val returnedError = expectMsgType[ResolutionFailed]
-    returnedError should be(expectedResolutionFailed)
+    val response = expectMsgType[ResolutionResult]
+    response should be(expectedResolutionFailed.failureNel[Artifact])
   }
 
   it should "return an error message if an exception happens when validating" in {
     val expectedException = new RuntimeException("TEST EXCEPTION")
+    val expectedResult = ExceptionThrown(expectedException)
 
     mockResolve expects (TestArtifactId, false) throwing expectedException
 
     moduleResolver ! Validate(TestArtifactId)
 
-    expectMsgType[ErrorResolvingModule].cause should be (expectedException)
+    expectMsgType[ResolutionResult] should be (expectedResult.failureNel[Artifact])
   }
 
   it should "return the artifact on successful acquiring of dependencies" in {
     val jobPackage = Artifact(TestArtifactId, Seq(new URL("http://www.google.com")))
 
-    mockResolve expects (TestArtifactId, true) returning Right(jobPackage)
+    mockResolve expects (TestArtifactId, true) returning jobPackage.successNel[ErrorResponse]
 
     moduleResolver ! Acquire(TestArtifactId)
 
-    val returnedPackage = expectMsgType[Artifact]
-    returnedPackage should be(jobPackage)
+    val response = expectMsgType[ResolutionResult]
+    response should be(jobPackage.successNel[ErrorResponse])
   }
 
   it should "return resolution failed error if it can't resolve dependencies" in {
-    val expectedResolutionFailed = UnresolvedDependencies(Seq("com.foo.bar"))
+    val expectedResolutionFailed = UnresolvedDependency(TestArtifactId)
 
-    mockResolve expects (TestArtifactId, true) returning Left(expectedResolutionFailed)
+    mockResolve expects (TestArtifactId, true) returning expectedResolutionFailed.failureNel[Artifact]
 
     moduleResolver ! Acquire(TestArtifactId)
 
-    val returnedError = expectMsgType[ResolutionFailed]
-    returnedError should be(expectedResolutionFailed)
+    val response = expectMsgType[ResolutionResult]
+    response should be(expectedResolutionFailed.failureNel[Artifact])
   }
 
   it should "return an error message if an exception happens" in {
     val expectedException = new RuntimeException("TEST EXCEPTION")
+    val expectedResult = ExceptionThrown(expectedException)
 
     mockResolve expects (TestArtifactId, true) throwing expectedException
 
     moduleResolver ! Acquire(TestArtifactId)
 
-    expectMsgType[ErrorResolvingModule].cause should be (expectedException)
+    expectMsgType[ResolutionResult] should be (expectedResult.failureNel[Artifact])
   }
 
 }
