9e56f115049d0984b27e1b20d7a9435d077ee4ee
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2016-04-14 20:24:06 +0100

    JSON Serialize Scalaz types Either & These. Encode these types in a way that prevents misterious "casting" in the serialize/deserialize cycle.

--- core/shared/src/main/scala/io/quckoo/serialization/json/ScalazJson.scala
+++ core/shared/src/main/scala/io/quckoo/serialization/json/ScalazJson.scala
@@ -1,45 +1,105 @@
 package io.quckoo.serialization.json
 
 import upickle.Js
-import upickle.default._
+import upickle.default.{Writer => JsonWriter, Reader => JsonReader, _}
 
 import scala.language.implicitConversions
-import scalaz.{Failure, NonEmptyList, Success, Validation}
+import scalaz._
 
 /**
   * Created by alonsodomin on 19/03/2016.
   */
 trait ScalazJson {
 
-  implicit def nonEmptyListW[T: Writer]: Writer[NonEmptyList[T]] = Writer[NonEmptyList[T]] {
+  // NonEmptyList
+
+  implicit def nonEmptyListW[T: JsonWriter]: JsonWriter[NonEmptyList[T]] = JsonWriter[NonEmptyList[T]] {
     x => Js.Arr(x.list.toList.map(writeJs(_)).toArray: _*)
   }
 
-  implicit def nonEmptyListR[T: Reader]: Reader[NonEmptyList[T]] = Reader[NonEmptyList[T]]( {
+  implicit def nonEmptyListR[T: JsonReader]: JsonReader[NonEmptyList[T]] = JsonReader[NonEmptyList[T]]( {
     case Js.Arr(x @ _*) =>
       val seq = x.map(readJs[T])
       NonEmptyList(seq.head, seq.tail: _*)
   })
 
-  implicit def validationW[E: Writer, A: Writer]: Writer[Validation[E, A]] = {
-    Writer[Validation[E, A]] {
-      case Failure(e) => Js.Arr(Js.Num(0), writeJs(e))
-      case Success(a) => Js.Arr(Js.Num(1), writeJs(a))
+  // Either \/
+
+  implicit def zeitherW[E: JsonWriter, A: JsonWriter]: JsonWriter[E \/ A] = {
+    JsonWriter[E \/ A] {
+      case -\/(e) => Js.Arr(Js.Num(10), writeJs(e))
+      case \/-(a) => Js.Arr(Js.Num(11), writeJs(a))
     }
   }
 
-  implicit def successW[E: Writer, A: Writer]: Writer[Success[A]] = Writer[Success[A]](validationW[E, A].write)
-  implicit def failureW[E: Writer, A: Writer]: Writer[Failure[E]] = Writer[Failure[E]](validationW[E, A].write)
+  implicit def zleftW[E: JsonWriter, A: JsonWriter]: JsonWriter[-\/[E]] =
+    JsonWriter[-\/[E]](zeitherW[E, A].write)
 
-  implicit def validationR[E: Reader, A: Reader]: Reader[Validation[E, A]] = Reader[Validation[E, A]](
-    successR[A].read orElse failureR[E].read
-  )
+  implicit def zrightW[E: JsonWriter, A: JsonWriter]: JsonWriter[\/-[A]] =
+    JsonWriter[\/-[A]](zeitherW[E, A].write)
 
-  implicit def successR[A: Reader]: Reader[Success[A]] = Reader[Success[A]] {
-    case Js.Arr(Js.Num(1), a) => Success[A](readJs[A](a))
+  implicit def zeitherR[E: JsonReader, A: JsonReader]: JsonReader[E \/ A] =
+    JsonReader[E \/ A](zrightR[A].read orElse zleftR[E].read)
+
+  implicit def zleftR[E: JsonReader]: JsonReader[-\/[E]] = JsonReader[-\/[E]] {
+    case Js.Arr(Js.Num(10), e) => -\/[E](readJs[E](e))
   }
-  implicit def failureR[E: Reader]: Reader[Failure[E]] = Reader[Failure[E]] {
-    case Js.Arr(Js.Num(0), e) => Failure[E](readJs[E](e))
+  implicit def zrightR[A: JsonReader]: JsonReader[\/-[A]] = JsonReader[\/-[A]] {
+    case Js.Arr(Js.Num(11), a) => \/-[A](readJs[A](a))
+  }
+
+  // These \&/
+
+  implicit def theseW[A: JsonWriter, B: JsonWriter]: JsonWriter[A \&/ B] = JsonWriter[A \&/ B] {
+    case \&/.This(a)    => Js.Arr(Js.Num(20), writeJs(a))
+    case \&/.That(b)    => Js.Arr(Js.Num(21), writeJs(b))
+    case \&/.Both(a, b) => Js.Arr(Js.Num(22), writeJs(a), writeJs(b))
+  }
+
+  implicit def thisW[A: JsonWriter, B: JsonWriter]: JsonWriter[\&/.This[A]] =
+    JsonWriter[\&/.This[A]](theseW[A, B].write)
+
+  implicit def thatW[A: JsonWriter, B: JsonWriter]: JsonWriter[\&/.That[B]] =
+    JsonWriter[\&/.That[B]](theseW[A, B].write)
+
+  implicit def bothW[A: JsonWriter, B: JsonWriter]: JsonWriter[\&/.Both[A, B]] =
+    JsonWriter[\&/.Both[A, B]](theseW[A, B].write)
+
+  implicit def theseR[A: JsonReader, B: JsonReader]: JsonReader[A \&/ B] =
+    JsonReader[A \&/ B](thisR[A].read orElse thatR[B].read orElse bothR[A, B].read)
+
+  implicit def thisR[A: JsonReader]: JsonReader[\&/.This[A]] = JsonReader[\&/.This[A]] {
+    case Js.Arr(Js.Num(20), a) => \&/.This[A](readJs[A](a))
+  }
+  implicit def thatR[B: JsonReader]: JsonReader[\&/.That[B]] = JsonReader[\&/.That[B]] {
+    case Js.Arr(Js.Num(21), b) => \&/.That[B](readJs[B](b))
+  }
+  implicit def bothR[A: JsonReader, B: JsonReader]: JsonReader[\&/.Both[A, B]] = JsonReader[\&/.Both[A, B]] {
+    case Js.Arr(Js.Num(22), a, b) => \&/.Both[A, B](readJs[A](a), readJs[B](b))
+  }
+
+  // Validation
+
+  implicit def validationW[E: JsonWriter, A: JsonWriter]: JsonWriter[Validation[E, A]] = {
+    JsonWriter[Validation[E, A]] {
+      case Failure(e) => Js.Arr(Js.Num(30), writeJs(e))
+      case Success(a) => Js.Arr(Js.Num(31), writeJs(a))
+    }
+  }
+
+  implicit def successW[E: JsonWriter, A: JsonWriter]: JsonWriter[Success[A]] =
+    JsonWriter[Success[A]](validationW[E, A].write)
+  implicit def failureW[E: JsonWriter, A: JsonWriter]: JsonWriter[Failure[E]] =
+    JsonWriter[Failure[E]](validationW[E, A].write)
+
+  implicit def validationR[E: JsonReader, A: JsonReader]: JsonReader[Validation[E, A]] =
+    JsonReader[Validation[E, A]](successR[A].read orElse failureR[E].read)
+
+  implicit def successR[A: JsonReader]: JsonReader[Success[A]] = JsonReader[Success[A]] {
+    case Js.Arr(Js.Num(31), a) => Success[A](readJs[A](a))
+  }
+  implicit def failureR[E: JsonReader]: JsonReader[Failure[E]] = JsonReader[Failure[E]] {
+    case Js.Arr(Js.Num(30), e) => Failure[E](readJs[E](e))
   }
 
 }
