85774fc4080a1c8db25692d4e34a560bf81f91f1
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2016-09-04 13:36:29 +0100

    Re-enable the test for the LoginForm

--- console/app/src/test/scala/io/quckoo/console/security/LoginTest.scala
+++ console/app/src/test/scala/io/quckoo/console/security/LoginTest.scala
@@ -3,10 +3,12 @@ package io.quckoo.console.security
 import japgolly.scalajs.react.Callback
 import japgolly.scalajs.react.test.ReactTestUtils
 
+import org.scalatest.FunSuite
+
 /**
   * Created by alonsodomin on 11/07/2016.
   */
-object LoginTest /*extends TestSuite {
+class LoginTest extends FunSuite {
   import LoginFormTestState._
   import LoginTestDsl._
 
@@ -24,8 +26,6 @@ object LoginTest /*extends TestSuite {
     invars
   }
 
-  //val handlerSubject = PublishSubject[(String, String)]()
-
   def runPlan(plan: dsl.Plan): Report[String] = {
 
     ReactTestUtils.withRenderedIntoDocument(LoginForm((_, _) => Callback.empty)) { comp =>
@@ -40,8 +40,7 @@ object LoginTest /*extends TestSuite {
     }
   }
 
-  override def tests = TestSuite {
-
+  test("should be able to submit user credentials") {
     val plan = Plan.action(
       setUsername("admin")
         >> setPassword("password")
@@ -52,4 +51,3 @@ object LoginTest /*extends TestSuite {
   }
 
 }
-*/
