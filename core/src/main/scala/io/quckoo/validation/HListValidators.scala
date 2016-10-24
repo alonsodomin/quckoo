f05b512bf3639605237e25fe8f79f39a346238da
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2016-10-24 08:37:23 +0100

    Attempt to use generic programing to derive Validators

--- core/src/main/scala/io/quckoo/validation/HListValidators.scala
+++ core/src/main/scala/io/quckoo/validation/HListValidators.scala
@@ -11,15 +11,14 @@ trait HListValidators {
 
   implicit def hnilValidator[F[_]: Applicative]: ValidatorK[F, HNil] = Validator.accept
 
-  implicit def hlistValidator[F[_]: Applicative, K <: Symbol, H, T <: HList](hValidator: ValidatorK[F, H])(
-    implicit witness: Witness.Aux[K], tValidator: ValidatorK[F, T]): ValidatorK[F, FieldType[K, H] :: T] = {
-    (hValidator * tValidator).dimap(hlist => (hlist.head, hlist.tail), _.map { case (h, t) => field[K](h) :: t })
+  implicit def hlistValidator[F[_]: Applicative, K <: Symbol, H, T <: HList](
+    implicit witness: Witness.Aux[K], hValidator: Lazy[ValidatorK[F, H]], tValidator: ValidatorK[F, T]): ValidatorK[F, FieldType[K, H] :: T] = {
+    (hValidator.value.at(witness.value.name) * tValidator).dimap(hlist => (hlist.head, hlist.tail), _.map { case (h, t) => field[K](h) :: t })
   }
 
-  implicit def genericValidator[F[_]: Applicative, A, R](validator: ValidatorK[F, R])(
-    implicit gen: LabelledGeneric.Aux[A, R]): ValidatorK[F, A] = {
-    def test(a: A) = gen.to(a)
-    ???
+  implicit def genericValidator[F[_]: Applicative, A, R](
+    implicit gen: LabelledGeneric.Aux[A, R], validator: Lazy[ValidatorK[F, R]]): ValidatorK[F, A] = {
+    validator.value.dimap(gen.to, _.map(gen.from))
   }
 
 }
