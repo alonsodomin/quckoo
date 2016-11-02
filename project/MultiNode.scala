7ae88177814a195e27efde6c13dd7149729a62f3
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2016-11-02 01:47:08 +0000

    Update scoverage runtime during MultiJVM tests

--- project/MultiNode.scala
+++ project/MultiNode.scala
@@ -9,7 +9,7 @@ object MultiNode {
   lazy val settings: Seq[Def.Setting[_]] = SbtMultiJvm.multiJvmSettings ++ Seq(
     libraryDependencies ++= Seq(
       "com.typesafe.akka" %% "akka-multi-node-testkit"  % Dependencies.version.akka.main,
-      "org.scoverage"     %% "scalac-scoverage-runtime" % "1.1.1"
+      "org.scoverage"     %% "scalac-scoverage-runtime" % "1.2.0"
     ).map(_ % MultiJvm),
     compile in MultiJvm := ((compile in MultiJvm) triggeredBy (compile in Test)).value,
     parallelExecution in Test := false,
