55cd6ae1d43e97660a894d14d07a8576209d668c
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2017-01-11 23:18:08 +0000

    Add Kryo serializers for JSR-310 backport instances.

---
+++ cluster/shared/src/main/scala/io/quckoo/serialization/kryo/KryoInitializer.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2016 Antonio Alonso Dominguez
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.quckoo.serialization.kryo
+
+import akka.actor.Props
+import com.esotericsoftware.kryo.Kryo
+import com.esotericsoftware.kryo.serializers.JavaSerializer
+import org.slf4s.Logging
+
+/**
+  * Created by alonsodomin on 11/01/2017.
+  */
+class KryoInitializer extends Logging {
+
+  def customize(kryo: Kryo): Unit = {
+    log.debug("Initializing custom Kryo serializers...")
+    TimeSerializers.addDefaultSerializers(kryo)
+    kryo.register(classOf[Props], new JavaSerializer)
+  }
+
+}
