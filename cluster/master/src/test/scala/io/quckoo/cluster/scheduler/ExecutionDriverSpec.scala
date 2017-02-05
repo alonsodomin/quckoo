12dab9e55be0e84f4cbb0dfbad77fc0b10bdc265
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2017-02-05 10:51:49 +0000

    Remove redundant typo

--- cluster/master/src/test/scala/io/quckoo/cluster/scheduler/ExecutionDriverSpec.scala
+++ cluster/master/src/test/scala/io/quckoo/cluster/scheduler/ExecutionDriverSpec.scala
@@ -200,7 +200,8 @@ class ExecutionDriverSpec extends TestKit(TestActorSystem("ExecutionDriverSpec")
       expectTerminated(executionDriver)
     }
 
-    "should self-passivate if restarted again" in {
+    "self-passivate if restarted again" in {
+      // Using the same actor name here to force a re-encarnation
       val reencarnatedDriver = system.actorOf(executionDriverProps, driverName)
       watch(reencarnatedDriver)
 
