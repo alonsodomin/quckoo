18fb1d0323b9f5bc695f834a91afd6632e48893b
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2017-04-07 18:12:30 +0100

    Run Multi-JVM tests separately

---
+++ scripts/travis-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sbt_cmd="sbt ++$TRAVIS_SCALA_VERSION"
+
+test_cmd="$sbt_cmd clean coverage test"
+validate_cmd="$sbt_cmd coverage master/multi-jvm:test"
+coverage_cmd="$sbt_cmd coverageReport && $sbt_cmd coverageAggregate"
+
+build_cmd="$test_cmd && $validate_cmd && $coverage_cmd"
+
+eval $build_cmd
